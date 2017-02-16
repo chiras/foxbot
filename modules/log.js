d9cf1e99377c0878de43852489e114970ca1c62f
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-02-16 01:12:51 +0100

    dev --> foxbot.js

--- modules/log.js
+++ modules/log.js
@@ -1,6 +1,8 @@
 module.exports = (msg, type, fs, logfile) => {
 	
-	var requesttext = msg.guild.name +  "\t" +  msg.author.username + "\t" + type + "\t" + msg.createdAt ;
-	fs.appendFile(logfile, requesttext, function (err) {});
+	var requesttext = msg.guild.name +  "\t" +  msg.author.username + "\t" + type + "\t" + msg.createdAt;
 	console.log(requesttext);
+	var requesttext = requesttext + "\n" ;
+	fs.appendFile(logfile, requesttext , function (err) {});
+
 };
