57c640486a58eff1b7392e13364dd0f846be3554
Alexander Keller <alexanderkeller@wvpn081.vpn.uni-wuerzburg.de>
2017-02-19 14:42:50 +0100

    help in one message

--- modules/help.js
+++ modules/help.js
@@ -1,9 +1,12 @@
 module.exports = (bot, msg, setitems) => {
-    msg.channel.sendMessage("Available options:");
-    msg.channel.sendMessage(" **!golden** -> Golden Vendor Items");
-    msg.channel.sendMessage(" **!pledges** -> Today's Pledges");
-    msg.channel.sendMessage(" **!status** -> ESO Server Status");
-    msg.channel.sendMessage(" **!set XXXX** -> Set item information (e.g. !set skel)" );
-    msg.channel.sendMessage(" **!twitch** -> Current top 5 ESO streams" );
+	var helpinfo = "";
+	helpinfo += "Available options:\n";
+    helpinfo += " **!golden** -> Golden Vendor Items\n";
+    helpinfo += " **!pledges** -> Today's Pledges\n";
+    helpinfo += " **!status** -> ESO Server Status\n";
+    helpinfo += " **!set XXXX** -> Set item information (e.g. !set skel)\n";
+    helpinfo += " **!twitch** -> Current top 5 ESO streams\n";
+    
+    msg.channel.sendMessage(helpinfo);
 
 };
