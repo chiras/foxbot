870d031d0165c527576cdc528a6f780fb631b6a0
Alexander Keller <alexanderkeller@wvpn081.vpn.uni-wuerzburg.de>
2017-02-19 14:29:25 +0100

    twitch in help and log

--- modules/help.js
+++ modules/help.js
@@ -4,5 +4,6 @@ module.exports = (bot, msg, setitems) => {
     msg.channel.sendMessage(" **!pledges** -> Today's Pledges");
     msg.channel.sendMessage(" **!status** -> ESO Server Status");
     msg.channel.sendMessage(" **!set XXXX** -> Set item information (e.g. !set skel)" );
+    msg.channel.sendMessage(" **!twitch** -> Current top 5 ESO streams" );
 
 };
