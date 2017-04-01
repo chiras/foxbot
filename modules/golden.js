9556b6de700e7ace2f63893d12266cc2bd9f797c
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-01 22:05:04 +0200

    fixed dates in luxury and golden, when month breaks

--- modules/golden.js
+++ modules/golden.js
@@ -1,31 +1,51 @@
 module.exports = (bot, msg, gsDayNames, request, cheerio) => {
+	function pad(n){return n<10 ? '0'+n : n}
 
     var goldenurl = "http://benevolentbowd.ca/games/esotu/esotu-chronicle-of-alliance-point-vendor-items/";
-    var dateObj = new Date();
+//    var dateObj = new Date();
     var goldentext = "";
 
-    var time = dateObj.getTime(); //months from 1-12
-    var monthtmp = dateObj.getUTCMonth() + 1; //months from 1-12
-    var month;
-    if (monthtmp < 10) {
-        month = "0" + monthtmp;
-    } else {
-        month = monthtmp;
-    }
+//     var time = dateObj.getTime(); //months from 1-12
+//     var monthtmp = dateObj.getUTCMonth() + 1; //months from 1-12
+//     var month;
+//     if (monthtmp < 10) {
+//         month = "0" + monthtmp;
+//     } else {
+//         month = monthtmp;
+//     }
 
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
+//     var day = dateObj.getUTCDate();
+//     var year = dateObj.getUTCFullYear();
+// 	
+//     newdate = year + "" + month;
+//     newdateday = day + "." + month + "." + year;
+//     newdateday2 = year + "-" + month + "-" + day;
+
+//     var d = new Date(newdateday2);
+//     var dayName = gsDayNames[d.getDay()];
+
+	var today = new Date();
+	var yesterday = new Date();
+	var preyesterday = new Date();
+    var dayName = gsDayNames[today.getDay()];
+
+	preyesterday.setDate(today.getDate() - 2); 
+	yesterday.setDate(today.getDate() - 1); 
 	
-    newdate = year + "" + month;
-    newdateday = day + "." + month + "." + year;
-    newdateday2 = year + "-" + month + "-" + day;
+	var lookupdates = [	preyesterday.getFullYear() + "-" + pad((preyesterday.getMonth()+1)) + "-" + pad(preyesterday.getDate()),	
+						yesterday.getFullYear() + "-" + pad((yesterday.getMonth()+1)) + "-" + pad(yesterday.getDate()),
+						today.getFullYear() + "-" + pad((today.getMonth()+1)) + "-" + pad(today.getDate())];
+ 
 
-    var d = new Date(newdateday2);
-    var dayName = gsDayNames[d.getDay()];
-
+//	console.log(yesterday + '/' + today)
 
     //   if (dayName == 'Friday' || dayName == 'Sunday' || dayName == 'Saturday'){
     if (dayName == 'Sunday' || dayName == 'Saturday') {
+    //	if (dayName == 'Sunday'){
+    //		var lookupdate = today;
+    //	}else{
+    //   	var lookupdate = yesterday;
+    //		}
     
         request(goldenurl, function(error, response, body) {
             if (error) {
@@ -36,19 +56,34 @@ module.exports = (bot, msg, gsDayNames, request, cheerio) => {
                 var $ = cheerio.load(body);
             }
 
-            $('h2[id="' + newdate + '"]')
-                .next('h3')
-                .each(function() {
-                    var $el = $(this);
-                    var datex = $(this).text()
-                    goldentext += "This weekend (" + datex + "), the Golden Vendor sells: \n";
-                })
-                .next('ul')
-                .find('li')
-                .each(function() {
-                    var $el = $(this);
-                    goldentext +=" * " + $(this).text() + "\n";
-                });
+           // $('h2[id="' + newdate + '"]')
+//             $('h3').text(lookupdate)
+           //     .next('h3')
+           //     .each(function() {
+           //         var $el = $(this);
+           //         var datex = $(this).text()
+                   goldentext += "This weekend, the Golden Vendor sells: \n";
+           //     })
+       //          .next('ul')
+//                 .find('li')
+//                 .each(function() {
+//                     var $el = $(this);
+//                     goldentext +=" * " + $(this).text() + "\n";
+//                 });
+            //   console.log(lookupdates);
+             
+                for (var i = 0, len = lookupdates.length; i < len; i++) {
+        			results = $('h3').filter(function() {
+  						return $(this).text().trim() === lookupdates[i] ;
+					}).next('ul')
+             	   .find('li')
+             	   .each(function() {
+             	       var $el = $(this);
+             	       goldentext +=" * " + $(this).text() + "\n";
+               		});
+				}
+
+
          msg.channel.sendMessage(goldentext);
        });
 
--- modules/luxury.js
+++ modules/luxury.js
@@ -1,31 +1,32 @@
 module.exports = (bot, msg, gsDayNames, request, cheerio) => {
+	function pad(n){return n<10 ? '0'+n : n}
 
     var luxuryurl = "http://benevolentbowd.ca/games/esotu/esotu-chronicle-of-luxury-furnisher-vendor-items/";
-    var dateObj = new Date();
+   // var dateObj = new Date();
     var luxurytext = "";
 
-    var time = dateObj.getTime(); //months from 1-12
-    var monthtmp = dateObj.getUTCMonth() + 1; //months from 1-12
-    var month;
-    if (monthtmp < 10) {
-        month = "0" + monthtmp;
-    } else {
-        month = monthtmp;
-    }
+	var today = new Date();
+	var yesterday = new Date();
+	var preyesterday = new Date();
+    var dayName = gsDayNames[today.getDay()];
 
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
+	preyesterday.setDate(today.getDate() - 2); 
+	yesterday.setDate(today.getDate() - 1); 
 	
-    newdate = year + "" + month;
-    newdateday = day + "." + month + "." + year;
-    newdateday2 = year + "-" + month + "-" + day;
+	var lookupdates = [	preyesterday.getFullYear() + "-" + pad((preyesterday.getMonth()+1)) + "-" + pad(preyesterday.getDate()),	
+						yesterday.getFullYear() + "-" + pad((yesterday.getMonth()+1)) + "-" + pad(yesterday.getDate()),
+						today.getFullYear() + "-" + pad((today.getMonth()+1)) + "-" + pad(today.getDate())];
+ 
 
-    var d = new Date(newdateday2);
-    var dayName = gsDayNames[d.getDay()];
-
+//	console.log(yesterday + '/' + today)
 
     //   if (dayName == 'Friday' || dayName == 'Sunday' || dayName == 'Saturday'){
     if (dayName == 'Sunday' || dayName == 'Saturday') {
+    //	if (dayName == 'Sunday'){
+    //		var lookupdate = today;
+    //	}else{
+    //   	var lookupdate = yesterday;
+    //		}
     
         request(luxuryurl, function(error, response, body) {
             if (error) {
@@ -36,26 +37,39 @@ module.exports = (bot, msg, gsDayNames, request, cheerio) => {
                 var $ = cheerio.load(body);
             }
 
-            $('h2[id="' + newdate + '"]')
-                .next('h3')
-                .each(function() {
-                    var $el = $(this);
-                    var datex = $(this).text()
-                    luxurytext += "This weekend (" + datex + "), the Luxury Vendor sells: \n";
-                })
-               // .next('pre')
-                .next('ul')
-                .find('li')
-                .each(function() {
-                    var $el = $(this);
-                    luxurytext +=" * " + $(this).text() + "\n";
-                });
+           // $('h2[id="' + newdate + '"]')
+//             $('h3').text(lookupdate)
+           //     .next('h3')
+           //     .each(function() {
+           //         var $el = $(this);
+           //         var datex = $(this).text()
+                   luxurytext += "This weekend, the Luxury Vendor sells: \n";
+           //     })
+       //          .next('ul')
+//                 .find('li')
+//                 .each(function() {
+//                     var $el = $(this);
+//                     luxurytext +=" * " + $(this).text() + "\n";
+//                 });
+            //   console.log(lookupdates);
+             
+                for (var i = 0, len = lookupdates.length; i < len; i++) {
+        			results = $('h3').filter(function() {
+  						return $(this).text().trim() === lookupdates[i] ;
+					}).next('ul')
+             	   .find('li')
+             	   .each(function() {
+             	       var $el = $(this);
+             	       luxurytext +=" * " + $(this).text() + "\n";
+               		});
+				}
+
+
          msg.channel.sendMessage(luxurytext);
        });
 
     } else {
         msg.channel.sendMessage("It's not weekend, nothing to sell. Sorry!");
     } // end else
-
 
 };
