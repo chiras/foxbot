839b723537aea979ea8b1e971911e0f885a8de00
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-05 22:06:17 +0200

    added weekly trials

---
+++ modules/trials.js
@@ -0,0 +1,56 @@
+module.exports = (bot, msg, request, cheerio, util) => {
+
+    var trialurl = "https://esoleaderboards.com"
+	var weeklytrials = [];
+	
+    request(trialurl, function(error, response, body) {
+        if (error) {
+            console.log("Error: " + error);
+        }
+
+        if (response.statusCode === 200) {
+            var $ = cheerio.load(body);
+        }
+	//	console.log(util.inspect($(this)));
+
+        $('tr[class="header"]').find('th > p ').filter(function() {
+  							return $(this).text().trim() === "Weekly Trials" ;
+  						}).parentsUntil('tr[class="header"]').next().next().children('td').children('strong').each(function() {
+  								weeklytrials.push($(this).text().trim());
+							//	console.log($(this).text().trim());
+
+  						});
+
+
+         //    .each(function() {
+//                 var $el = $(this);
+//                 if ($(this).text().substring(0, 5) == " Next") {
+//                     msg.channel.sendMessage($(this).text());
+//                 }
+            //}
+           // );
+
+    msg.channel.sendMessage("This weeks trials are " + weeklytrials[0] + " (EU) and "  + weeklytrials[1] + " (US)" );
+    });
+
+};
+
+
+// 
+//                 <tr class='header'>
+//                   <th colspan='4' style='font-size: 15px;  color: #FFF; text-align: center; padding: 20px;'>   <p class='type' align='center'>  Weekly Trials </p> </th>
+//                 </tr>
+//                 <tr class='header'>
+// 
+//                   <th scope='col' style='font-size: 15px; text-align: center; padding: 20px;' colspan='2'> EU Megaserver </th>
+// 
+// 
+//                   <th scope='col' style='font-size: 15px; text-align: center; padding: 20px;' colspan='2'> NA Megaserver </th></tr>
+// 
+//                 <!-- NEW ROW-->
+//                 <tr class='row1' >
+//                   <td class='normal' style='font-size:20px; text-align: center; padding: 10px;' width='50%' colspan='2'><strong>Aetherian Archive</strong></br> <a href='trial_sample.php?trial=weekly'><img src='img/aa.png' width='200px''></a>
+// 
+//                   </td>
+//                   <td class='normal' style='font-size:20px; text-align: center;' width='50%' colspan='2'><strong>Sanctum Ophidia</strong></br><a href='trial_sample.php?trial=weekly'><img src='img/so.png' width='200px''></a>
+// 
