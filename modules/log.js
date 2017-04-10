cc75c485f3a57e706c6c2a97b072014f649d10a1
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-10 22:33:22 +0200

    logging in special fox channel

--- modules/log.js
+++ modules/log.js
@@ -1,8 +1,10 @@
-module.exports = (msg, type, fs, logfile) => {
+module.exports = (msg, type, fs, logfile, bot) => {
 	
 	var requesttext = msg.guild.name +  "\t" +  msg.author.username + "\t" + type + "\t" + msg.createdAt;
 	console.log(requesttext);
 	var requesttext = requesttext + "\n" ;
 	fs.appendFile(logfile, requesttext , function (err) {});
+	
+	bot.channels.get("301074654301388800").sendMessage(requesttext)
 
 };
