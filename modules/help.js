5fc0f817ee72e444672df0e1f86d2fdc63d36e1b
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-11 22:22:39 +0200

    embedded help

--- modules/help.js
+++ modules/help.js
@@ -1,29 +1,49 @@
 module.exports = (bot, msg, setitems) => {
-	var helpinfo = "";
- 	//helpinfo += "\n\n..................................................................................................."
-	helpinfo += "\nAvailable commands are:\n";
- 	helpinfo += "..................................................................................................."
-    helpinfo += "\n ### **Regular Events ** \n\n";
-    helpinfo += " **!pledges**      -> Today's Undaunted pledges\n";
-    helpinfo += " **!trials**      -> This weeks's special trials\n";
- 	helpinfo += "..................................................................................................."
-    helpinfo += "\n ### **Weekly Events **\n\n";
-    helpinfo += " **!golden**      -> Cyrodiil's Golden Vendor Items\n";
-    helpinfo += " **!luxury**      -> Luxury Housing Vendor Items\n";
- 	helpinfo += "..................................................................................................."
-    helpinfo += "\n ### **Useful game information **\n\n"; 
-    helpinfo += " **!status**      -> ESO server status\n";
-    helpinfo += " **!patch**      -> Latest ESO patch notes\n";
-    helpinfo += " **!set SETNAME**      -> Set item information (e.g. !set skel)\n";
- 	helpinfo += "..................................................................................................."
-    helpinfo += "\n ### **Media **\n\n"; 
-    helpinfo += " **!youtube**      -> Hot and new ESO videos\n";
-    helpinfo += " **!twitch**      -> Current top 5 ESO streams\n";
- 	helpinfo += "..................................................................................................."
-    helpinfo += "\n ### **Discord Bot **\n\n"; 
-    helpinfo += " **!contact**      -> Contact information for the Bot author\n";
- 	helpinfo += "..................................................................................................."
-    
-    msg.channel.sendMessage(helpinfo);
 
+    var eventinfo = "**!pledges**      -> Today's Undaunted pledges\n";
+    eventinfo += "**!trials**      -> This weeks's special trials\n";
+
+    var weekendinfo = "**!golden**      -> Cyrodiil's Golden Vendor Items\n";
+    weekendinfo += "**!luxury**      -> Luxury Housing Vendor Items\n";
+
+    var offinfo = "**!status**      -> ESO server status\n";
+    offinfo += "**!patch**      -> Latest ESO patch notes\n";
+
+    var iteminfo = " **!set SETNAME**      -> Set item information (e.g. !set skel)\n";
+
+    var mediainfo = "**!youtube**      -> Hot and new ESO videos\n";
+    mediainfo += "**!twitch**      -> Current top 5 ESO streams\n";
+
+    var botinfo = "**!contact**      -> Contact information for the Bot author\n";
+    botinfo += "**!help**      -> This help page\n";
+    
+    msg.channel.sendEmbed({
+  			color: 0x800000,
+  		//	description: helpinfo,
+  			fields: [{
+       			 name: 'Regular Events',
+       			 value: eventinfo
+     		 },
+     		 {
+       			 name: 'Weekend Vendors',
+       			 value: weekendinfo
+			},
+      		{
+      			 name: 'Itemization',
+        		 value: iteminfo
+      		},
+      		{
+      			 name: 'Official Information',
+        		 value: offinfo
+      		},
+      		{
+      			 name: 'Media',
+        		 value: mediainfo
+      		},
+      		{
+      			 name: 'Bot',
+        		 value: botinfo
+      		}
+    		]
+		});
 };
