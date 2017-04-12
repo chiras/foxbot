b1a11581efdad1b4b5405fa97e0120d908b17c96
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-12 18:43:04 +0200

    twitch update

--- modules/twitch.backup.js
+++ modules/twitch.backup.js
@@ -1,56 +1,13 @@
-module.exports = (bot, msg, twitch,util, request) => {  
-        var clientid = "ppswltywns8ixv0bdnesh852j9qn2g"
-        var twitchurl = "https://api.twitch.tv/kraken/streams?game=The%20Elder%20Scrolls%20Online&live=true&client_id="+clientid;
-
-//         msg.channel.sendEmbed({
-//   			color: 0x800000,
-//   		//	description: helpinfo,
-//   			fields: [{
-//        			 name: 'Twitch',
-//        			 value: "Is currently disabled due to some changes in the twitch API"
-//      		 }
-//     		]
-// 		});
-		
-		request({
-    		url: twitchurl,
-        	json: true
-	    }, function(error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-//		console.log(body);
-
-			var twitchout = "";
-  			var maxcount = 5;
- 			var curcount = 1;
-
-  			for (var i = 0; curcount <= 5; i++){
-  			  	if (body.streams[i]){
-  			  		twitchout += "\n" + curcount + ". **[" + JSON.stringify(body.streams[i].channel.display_name).replace(/\"/g, "") + "](http://twitch.tv/"+body.streams[i].channel.display_name+")** " + "(" + JSON.stringify(body.streams[i].viewers) + ' Viewers): ' + JSON.stringify(body.streams[i].channel.status) +'\n';
-					curcount++;
-				}
-  			}
-//  			console.log(twitchout);		
-		
-		msg.channel.sendEmbed({
+module.exports = (bot, msg, twitch,util) => {  
+           msg.channel.sendEmbed({
   			color: 0x800000,
   		//	description: helpinfo,
   			fields: [{
-       			 name: 'Hot ESO streams online on Twitch.tv are:',
-       			 value: twitchout
+       			 name: 'Twitch',
+       			 value: "Is currently disabled due to some changes in the twitch API"
      		 }
     		]
 		});
-		
-        }else{
-          msg.channel.sendMessage("Sorry there was an unexpected connection error, please try again later." );
-
-        }
-    })
-		
-		
-		//https://api.twitch.tv/kraken/streams?game=The%20Elder%20Scrolls%20Online&live=true
-		
 		  
 //    twitch.searchStreams({query: "The+Elder+Scrolls+Online", limit: 10}, function(err, body){
 //   	  if (err){
