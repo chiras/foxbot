c889321d65e985713ca274ebda25b66340e5464b
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-12 12:38:49 +0200

    revert due to twitch issues

--- modules/twitch.js
+++ modules/twitch.js
@@ -1,35 +1,44 @@
-module.exports = (bot, msg, twitch, util) => {
+module.exports = (bot, msg, twitch,util) => {  
+     
+   twitch.searchStreams({query: "The+Elder+Scrolls+Online", limit: 10}, function(err, body){
+  	  if (err){
+          msg.channel.sendMessage("Sorry there was an unexpected connection error, please try again later." );
+    	  console.log(err);
+    	} else {
+ 	 		var twitchout = "";//\n\n..................................................................................................."
+    		 //twitchout += "\n### ** Hot ESO streams online on Twitch.tv are:\n**"
+  	//		msg.channel.sendMessage("Twitching" + util.inspect(body));
+  			
+  			var maxcount = 5;
+ 			var curcount = 1;
 
-    twitch.searchStreams({
-        query: "The+Elder+Scrolls+Online",
-        limit: 10
-    }, function(err, body) {
-        if (err) {
-            msg.channel.sendMessage("Sorry there was an unexpected connection error, please try again later.");
-            console.log(err);
-        } else {
-            var twitchout = "";
-            var maxcount = 5;
-            var curcount = 1;
+  			for (var i = 0; curcount <= 5; i++){
+  			  	if (body.streams[i]){
+  			  		twitchout += "\n" + curcount + ". **[" + JSON.stringify(body.streams[i].channel.display_name).replace(/\"/g, "") + "](http://twitch.tv/"+body.streams[i].channel.display_name+")** " + "(" + JSON.stringify(body.streams[i].viewers) + ' Viewers): ' + JSON.stringify(body.streams[i].channel.status) +'\n';
+					curcount++;
+				}
+  			}
+  			
+  			
+  	//		twitchout += "1. **" + JSON.stringify(body.streams[0].channel.display_name).replace(/\"/g, "") + "**: " + "(" + JSON.stringify(body.streams[0].viewers) + ' Viewers): "http://twitch.tv/'+body.streams[0].channel.display_name+'"\n' + JSON.stringify(body.streams[0].channel.status) +'"\n\n';
+  	//		twitchout += "2. **" + JSON.stringify(body.streams[1].channel.display_name).replace(/\"/g, "") + "**: " + "(" + JSON.stringify(body.streams[1].viewers) + ' Viewers): "http://twitch.tv/'+body.streams[1].channel.display_name+'"\n' + JSON.stringify(body.streams[1].channel.status) +'"\n\n';
+  	//		twitchout += "3. **" + JSON.stringify(body.streams[2].channel.display_name).replace(/\"/g, "") + "**: " + "(" + JSON.stringify(body.streams[2].viewers) + ' Viewers): "http://twitch.tv/'+body.streams[2].channel.display_name+'"\n' + JSON.stringify(body.streams[2].channel.status) +'"\n\n';
+  	//		twitchout += "4. **" + JSON.stringify(body.streams[3].channel.display_name).replace(/\"/g, "") + "**: " + "(" + JSON.stringify(body.streams[3].viewers) + ' Viewers): "http://twitch.tv/'+body.streams[3].channel.display_name+'"\n' + JSON.stringify(body.streams[3].channel.status) +'"\n\n';
+  	//		twitchout += "5. **" + JSON.stringify(body.streams[4].channel.display_name).replace(/\"/g, "") + "**: " + "(" + JSON.stringify(body.streams[4].viewers) + ' Viewers): "http://twitch.tv/'+body.streams[4].channel.display_name+'"\n' + JSON.stringify(body.streams[4].channel.status) +'"\n\n';
+  			
+ 			//twitchout += "..................................................................................................."
+			//msg.channel.sendMessage(twitchout);
+			
+        msg.channel.sendEmbed({
+  			color: 0x800000,
+  		//	description: helpinfo,
+  			fields: [{
+       			 name: 'Hot ESO streams online on Twitch.tv are:',
+       			 value: twitchout
+     		 }
+    		]
+		});
 
-            for (var i = 0; curcount <= 5; i++) {
-                if (body.streams[i]) {
-                    twitchout += "\n" + curcount + ". **[" + JSON.stringify(body.streams[i].channel.display_name).replace(/\"/g, "") + "](http://twitch.tv/" + body.streams[i].channel.display_name + ")** " + "(" + JSON.stringify(body.streams[i].viewers) + ' Viewers): ' + JSON.stringify(body.streams[i].channel.status) + '\n';
-                    curcount++;
-                }
-            }
-
-
-
-            msg.channel.sendEmbed({
-                color: 0x800000,
-                fields: [{
-                    name: 'Hot ESO streams online on Twitch.tv are:',
-                    value: twitchout
-                }]
-            });
-
-        }
-    });
-
+   	 }});
+    
 };
