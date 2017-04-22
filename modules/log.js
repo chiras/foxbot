d0e16a0a8324f3c97d5006703ba6cded96851878
chiras <ciya.the.fox@gmail.com>
2017-04-22 06:28:19 +0200

    avoid error guild name

--- modules/log.js
+++ modules/log.js
@@ -1,6 +1,21 @@
 module.exports = (msg, type, fs, logfile, bot) => {
 	
-	var requesttext = msg.guild.name +  "\t" +  msg.author.username + "\t" + type + "\t" + msg.createdAt;
+	var requesttext = ""
+	
+	if (msg.guild){requesttext = msg.guild.name}else{
+	requesttext = "unknown guild"}
+	
+	requesttext +  "\t"
+	
+	if (msg.author){requesttext += msg.author.username}else{
+	requesttext += "unknown author"}	
+
+	requesttext += "\t" + type
+	
+	if (msg.createdAt){requesttext += msg.createdAt}else{
+	requesttext += "unknown time"}	
+		
+//	var requesttext = tmp +  "\t" +  tmp2 + "\t" + type + "\t" + msg.createdAt;
 	console.log(requesttext);
 	var requesttext = requesttext + "\n" ;
 	fs.appendFile(logfile, requesttext , function (err) {});
