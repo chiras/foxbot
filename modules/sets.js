94cda1e1028b085ea065a7dd796de201ab399f67
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-04-22 16:11:20 +0200

    set in embed style

--- modules/sets.js
+++ modules/sets.js
@@ -9,39 +9,84 @@ module.exports = (bot, msg, setitems) => {
                 results.push(setitems[i]);
             }
         }
+		var setTextTitle = "Found " + results.length + " sets matching your request."; 
+	//	msg.channel.sendMessage("Found " + results.length + " sets matching your request.");
 
-		msg.channel.sendMessage("Found " + results.length + " sets matching your request.");
+        msg.channel.sendEmbed({
+  			color: 0x800000,
+  			description: setTextTitle,
+		});		
 		
-		if (results.length < 6){
+		if (results.length < 6 && results.length > 0){
+        var outputsets = [];
+            
         for (var i = 0; i < results.length; i++) {
 
-            var outputsets = "";
 
-            outputsets += "**" + JSON.stringify(results[i].Name) + "** (" + JSON.stringify(results[i].Pieces) + ")" + " obtainable from ";
-            outputsets += JSON.stringify(results[i].Location) + " (" + JSON.stringify(results[i].Type) + ")\n";
+            outputsets[JSON.stringify(results[i].Name)] =  JSON.stringify(results[i].Pieces) + "," + " obtainable from ";
+            outputsets[JSON.stringify(results[i].Name)] += JSON.stringify(results[i].Location) + " (" + JSON.stringify(results[i].Type) + ")\n";
             if (results[i].i1 != "") {
-                outputsets += "(1 pc) " + JSON.stringify(results[i].i1) + "\n"
+                outputsets[JSON.stringify(results[i].Name)] += "**(1 pc)** " + JSON.stringify(results[i].i1) + "\n"
             };
             if (results[i].i2 != "") {
-                outputsets += "(2 pc) " + JSON.stringify(results[i].i2) + "\n"
+                outputsets[JSON.stringify(results[i].Name)] += "**(2 pc)** " + JSON.stringify(results[i].i2) + "\n"
             };
             if (results[i].i3 != "") {
-                outputsets += "(3 pc) " + JSON.stringify(results[i].i3) + "\n"
+                outputsets[JSON.stringify(results[i].Name)] += "**(3 pc)** " + JSON.stringify(results[i].i3) + "\n"
             };
             if (results[i].i4 != "") {
-                outputsets += "(4 pc) " + JSON.stringify(results[i].i4) + "\n"
+                outputsets[JSON.stringify(results[i].Name)] += "**(4 pc)** " + JSON.stringify(results[i].i4) + "\n"
             };
             if (results[i].i5 != "") {
-                outputsets += "(5 pc) " + JSON.stringify(results[i].i5) + "\n"
+                outputsets[JSON.stringify(results[i].Name)] += "**(5 pc)** " + JSON.stringify(results[i].i5) + "\n"
             };
 
-            var outputsetsesc = outputsets.replace(/\"/g, "");
+        msg.channel.sendEmbed({
+  			color: 0x800000,
+  		//	description: helpinfo,
+  			fields: [{
+       			 name: Object.keys(outputsets)[i].replace(/\"/g, ""),
+       			 value: outputsets[Object.keys(outputsets)[i]].replace(/\"/g, "")
+     		 }
+    		]
+		});
 
-            msg.channel.sendMessage(outputsetsesc);
+         //   var outputsetsesc = outputsets.replace(/\"/g, "");
+
+         //   msg.channel.sendMessage(outputsetsesc);
 
         }		
+		}else if(results.length == 0){
+	//		msg.channel.sendMessage("Please avoid metacharacters like hyphens or apostrophes. Tip: you can search for incomplete names as well e.g.\n**!set skel**\nwill prompt also sets matching Skeleton.");
+        msg.channel.sendEmbed({
+  			color: 0x800000,
+  			description: "Please avoid metacharacters like apostrophes. \nTip: you can search for incomplete names as well e.g.\n**!set skel**\nwill prompt also sets matching 'Skeleton'.",
+		});		
 		}else{
-			msg.channel.sendMessage("Please narrow down your request to avoid spam by providing more characters.");
+	//		msg.channel.sendMessage("Please narrow down your request to avoid spam by providing more characters.");
+        msg.channel.sendEmbed({
+  			color: 0x800000,
+  			description: "Please narrow down your request to avoid spam by providing more characters.",
+		});		
 		}
+		
+// 		
+//         msg.channel.sendEmbed({
+//   			color: 0x800000,
+//   		//	description: helpinfo,
+//   			fields: [{
+//        			 name: Object.keys(outputsets)[0].replace(/\"/g, ""),
+//        			 value: outputsets[Object.keys(outputsets)[0]].replace(/\"/g, "")
+//      		 },
+//      		 {
+//        			 name: Object.keys(outputsets)[1].replace(/\"/g, ""),
+//        			 value: outputsets[Object.keys(outputsets)[1]].replace(/\"/g, "")
+// 			},
+//      		 {
+//        			 name: Object.keys(outputsets)[2],
+//        			 value: outputsets[Object.keys(outputsets)[2]]
+// 			}
+//     		]
+// 		});
 
 };
