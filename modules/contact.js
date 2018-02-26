6b8836f96620b0acbfce33ef0be6a047197ac2c3
Alexander Keller <a.keller@biozentrum.uni-wuerzburg.de>
2018-02-26 08:57:05 +0100

    Added code contribution names

--- modules/contact.js
+++ modules/contact.js
@@ -7,7 +7,8 @@ module.exports = (bot, msg, options, Discord) => {
 		embed.setTitle("Contact options")
 		embed.addField("Discord Server", "Join the Fox-Server and contact me there: https://discord.gg/xY4jwrQ")
 		embed.addField("Discord DM", "Fox#6800 (or directly <@218803587491299328> if we share a server)")
-		embed.addField("Ingame", "EU-PC: @chi-ras")
+		embed.addField("Ingame", "EU-PC: @it's.fox")
+		embed.addField("Code contribution", "EU-PC: @it's.fox, @woeler; NA-PC: @seri")
 		embed.addField("ESO Forums", "Join the [Forum discussion](https://forums.elderscrollsonline.com/en/discussion/334414/discord-bot-with-eso-information/p1)")
 					
         mh.send(msg, embed, options);
