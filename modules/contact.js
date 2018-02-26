89a231c66f77a53ee389067b48a0771604445f69
Alexander Keller <a.keller@biozentrum.uni-wuerzburg.de>
2018-02-26 08:57:52 +0100

    Update contact.js

--- modules/contact.js
+++ modules/contact.js
@@ -5,9 +5,7 @@ module.exports = (bot, msg, options, Discord) => {
 		var embed = mh.prepare(Discord)
 		
 		embed.setTitle("Contact options")
-		embed.addField("Discord Server", "Join the Fox-Server and contact me there: https://discord.gg/xY4jwrQ")
-		embed.addField("Discord DM", "Fox#6800 (or directly <@218803587491299328> if we share a server)")
-		embed.addField("Ingame", "EU-PC: @it's.fox")
+		embed.addField("Discord Server", "Join the Fox-Server and ping a dev there: https://discord.gg/xY4jwrQ")
 		embed.addField("Code contribution", "EU-PC: @it's.fox, @woeler; NA-PC: @seri")
 		embed.addField("ESO Forums", "Join the [Forum discussion](https://forums.elderscrollsonline.com/en/discussion/334414/discord-bot-with-eso-information/p1)")
 					
