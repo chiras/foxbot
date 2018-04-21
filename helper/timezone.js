58ca8a38fa1c1518b3b489cda2e636eaec471c20
Scott Thomas <s@sthomas.id.au>
2018-04-21 18:34:55 +0930

    Time: Add an override for EST to refer to EST5EDT
    Pure 'EST' does not have daylight time, and is used for some islands near the equator, while majority of ESO population would probably be after US/Eastern for daily reset times, forum maint announcements, etc.

--- helper/timezone.js
+++ helper/timezone.js
@@ -1,11 +1,17 @@
 const moment = require('moment-timezone');
 
-exports.searchZone = function(zone) {
+exports.searchZone = function (zone) {
+    var z = zone.toLowerCase();
+
+    // We probably want US EST, not the non-DST obeying EST (used in some islands near the equator)
+    if (z == 'est')
+        return ['EST5EDT'];
+
     var matches = [];
     var exact = '';
     if (zone.length >= 3)
         moment.tz.names().forEach(function (val) {
-            if (val.toLowerCase() == zone.toLowerCase())
+            if (val.toLowerCase() == zone.z)
                 exact = val;
 
             if (val.toLowerCase().indexOf(zone.toLowerCase()) >= 0)
